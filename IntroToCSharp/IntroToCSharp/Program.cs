b04e2a397912a49e9e3c61d2974c564ea95371f2
Cade Anderson <cadeanderson99@gmail.com>
2019-01-31 09:27:24 -0800

    Inital Commit for IntroToCSharp

---
+++ IntroToCSharp/IntroToCSharp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] blacklist = { "carter", "dick", "voldimort", "fuck"};
+
+            Console.WriteLine("Please enter a username");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("---Welcome to Chat.exe---");
+            /*for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }*/
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                for (int i = 0; i < blacklist.Length; i++)
+                {
+                    if (input.ToLower().Contains(blacklist[i]))
+                    {
+                        string replacement = "";
+                        for (int j = 0; j < blacklist[i].Length; j++)
+                        {
+                            replacement = string.Concat(replacement, "*");
+                        }
+                        
+                        input = input.Replace(blacklist[i], replacement);
+                    }
+                }
+
+                Console.WriteLine("<" + username + "> " + input);
+            }
+            
+            
+        }
+    }
+}
