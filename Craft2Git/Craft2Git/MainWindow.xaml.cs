c4843c9877e61d91080c0d08156dbee5481e3154
Edacth <cadeanderson99@gmail.com>
2019-02-13 19:53:42 -0800

    Created the git2craft project

---
+++ Craft2Git/Craft2Git/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Craft2Git
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public class MyData : ObservableCollection<string>
+    {
+        public MyData()
+        {
+            Add("Item 1");
+            Add("Item 2");
+            
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyData listData = new MyData();
+            Binding binding1 = new Binding();
+
+            binding1.Source = listData;
+            list_left.SetBinding(ListBox.ItemsSourceProperty, binding1);
+        }
+
+        private void OnClickLeft(object sender, RoutedEventArgs e)
+        {
+            ////list_left.Items.
+        }
+    }
+}
