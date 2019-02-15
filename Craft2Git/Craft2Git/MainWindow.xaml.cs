f42ab59dd136dfcefc4a9f089cf7875e60b9a3c5
Cade Anderson <cadeanderson99@gmail.com>
2019-02-15 12:58:41 -0800

    Git2Craft finds and parses manifest.json now

--- Craft2Git/Craft2Git/MainWindow.xaml.cs
+++ Craft2Git/Craft2Git/MainWindow.xaml.cs
@@ -13,48 +13,41 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Craft2Git;
+using Newtonsoft;
 
 namespace Craft2Git
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    /// 
-    public class addonEntry
+    public class PackList : ObservableCollection<PackEntry>
     {
-        private string name;
-        private string filePath;
-
-        public string Name { get => name; set => name = value; }
-        public string FilePath { get => filePath; set => filePath = value; }
-
-        public addonEntry(string _name, string _filePath)
+        public PackList()
         {
-            name = _name;
-            filePath = _filePath;
-        }
-    }
-
-    public class addonList : ObservableCollection<addonEntry>
-    {
-        public addonList()
-        {
-            addonEntry entry1 = new addonEntry("Arena", "C://");
-            Add(entry1);
-            Add(new addonEntry("Battle", "C://"));
+          
             
         }
     }
 
+
     public partial class MainWindow : Window
     {
+        PackList listData;
+        string defaultFilePath = "C:/Users/s189062/Desktop/Addon Source/";
+        string filePath = "C:/Users/s189062/Desktop/Addon Source/";
+
         public MainWindow()
         {
-            InitializeComponent();
-            addonList listData = new addonList();
+            
+            listData = new PackList();
             Binding binding1 = new Binding();
-
             binding1.Source = listData;
+            
+            InitializeComponent();
+
+            txtbox_left.Text = defaultFilePath;
+
+            loadPacks(filePath);
+
             list_left.SetBinding(ListBox.ItemsSourceProperty, binding1);
         }
 
@@ -62,6 +55,56 @@ namespace Craft2Git
         {
             ////list_left.Items.
             //txtbox_left.Text = list_left.Items[0];
+            PackEntry entry1 = loadPack(filePath);
+            listData.Add(entry1);
+        }
+
+        public PackEntry loadPack(string filePath)
+        {
+            string contents = File.ReadAllText(filePath);
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<PackEntry>(contents);
+
+        }
+
+        public void loadPacks(string filePath)
+        {
+            string[] subDirectories;
+            try
+            {
+                subDirectories = Directory.GetDirectories(filePath);
+            }
+            catch (Exception)
+            {
+
+                return;
+            }
+            for (int i = 0; i < subDirectories.Length; i++)
+            {
+                string filePathAppended = subDirectories[i] + "/manifest.json";
+                if (File.Exists(filePathAppended))
+                {
+                    
+                    string contents = File.ReadAllText(filePathAppended);
+                    PackEntry newEntry = Newtonsoft.Json.JsonConvert.DeserializeObject<PackEntry>(contents);
+                    newEntry.filePath = filePathAppended;
+
+                    newEntry.iconPath = subDirectories[i] + "/pack_icon.png";
+
+                    listData.Add(newEntry);
+                }
+            }
+        }
+
+        private void txtbox_left_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           filePath = txtbox_left.Text;
+            while (listData.Count >= 1)
+            {
+                listData.RemoveAt(0);
+            }
+            loadPacks(filePath);
         }
     }
+
+    
 }
---
+++ Craft2Git/Craft2Git/PackEntry.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Craft2Git
+{
+
+    public class PackEntry
+    {
+        public int format_version { get; set; }
+        public Header header { get; set; }
+        public Module[] modules { get; set; }
+        public Dependency[] dependencies { get; set; }
+        public string filePath { get; set; }
+        public string iconPath { get; set;} 
+        //public PackEntry(string _name)
+        //{
+        //    //header.name = new string;
+        //    header.name = _name;
+        //}
+    }
+
+    public class Header
+    {
+        public string description { get; set; }
+        public string name { get; set; }
+        public string uuid { get; set; }
+        public int[] version { get; set; }
+    }
+
+    public class Module
+    {
+        public string description { get; set; }
+        public string type { get; set; }
+        public string uuid { get; set; }
+        public int[] version { get; set; }
+    }
+
+    public class Dependency
+    {
+        public string uuid { get; set; }
+        public int[] version { get; set; }
+    }
+
+}
